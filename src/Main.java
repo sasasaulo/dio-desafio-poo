81cdbf16c8945329f47cb2fe9e54f23c6ae79f59
Saulo Scarpina <saulo@dti-n-258035.adm.spo.ifsp.edu.br>
2022-03-18 15:58:57 -0300

    Mentoria e curso antes da a refatoração

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Javascript");
+        curso2.setDescricao("Descrição curso Javascript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria("Mentoria de Java", "Descrição mentoria Java", LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
