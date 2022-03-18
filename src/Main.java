6f666d40b7ea6d4a801bd853de79998a3df05906
Saulo Scarpina <saulo@dti-n-258035.adm.spo.ifsp.edu.br>
2022-03-18 18:35:53 -0300

    Construindo Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -17,9 +19,28 @@ public class Main {
 
         Mentoria mentoria = new Mentoria("Mentoria de Java", "Descrição mentoria Java", LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev saulo = new Dev();
+        saulo.setNome("Saulo");
+        saulo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Saulo: " + saulo.getConteudosInscritos());
+
+        System.out.println("Conteúdos concluídos Saulo: " + saulo.getConteudosConcluidos());
+
+        Dev maria = new Dev();
+        maria.setNome("Maria");
+        maria.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Maria: " + maria.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Maria: " + maria.getConteudosConcluidos());
     }
 }
